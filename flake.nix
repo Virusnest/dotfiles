022711f44020785bf352499c512f9ad508a898d1
Virusnest <virusnest@github.com>
2026-02-16 22:06:44 +1100

    Auto Modules

--- flake.nix
+++ flake.nix
@@ -6,15 +6,22 @@
   };
 
 outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-  in {
-    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-      inherit system;
+let
+  system = "x86_64-linux";
 
-      modules = [
-        ./hosts/laptop/configuration.nix
-      ];
-    };
+  mkModuleList = dir:
+    map (name: dir + "/${name}")
+    (builtins.filter
+      (name: builtins.match ".*\\.nix" name != null)
+      (builtins.attrNames (builtins.readDir dir)));
+in
+{
+  nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+    inherit system;
+
+    modules =
+      [ ./hosts/laptop/configuration.nix ]
+      ++ mkModuleList ./modules;
   };
+};
 }
