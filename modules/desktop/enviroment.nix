0741a1dbc4f80b5cae54d161b5d3bd778d6929cb
Virusnest <virusnest@github.com>
2026-02-17 08:56:51 +1100

    update

--- modules/desktop/enviroment.nix
+++ modules/desktop/enviroment.nix
@@ -1,1 +1,31 @@
-{}
+{ config, pkgs, ... }:
+
+{
+ programs.hyprland = {
+  enable = true;
+  xwayland.enable = true;
+ };
+ environment.sessionVariables = {
+  NIXOS_OZONE_WL="1";
+ };
+
+ environment.systemPackages = with pkgs; [
+  kitty
+  rofi
+  quickshell
+  waybar
+ ];
+ services.dbus.enable = true;
+ xdg.portal = {
+  enable = true;
+  extraPortals = [pkgs.xdg-desktop-portal];
+ };
+ security.rtkit.enable = true;
+ services.pipewire = {
+  enable = true;
+  alsa.enable = true;
+  alsa.support32Bit = true;
+  pulse.enable = true;
+  jack.enable = true;
+ };
+}
--- modules/system/virtualisation.nix
+++ modules/system/virtualisation.nix
@@ -1,1 +1,52 @@
-{}
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.my.virtualisation;
+in
+{
+  options.my.virtualisation = {
+    enable = lib.mkEnableOption "virtualisation stack";
+
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = "virusnest";
+      description = "User added to libvirtd group";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+
+    virtualisation.libvirtd = {
+      enable = true;
+
+      qemu = {
+        package = pkgs.qemu_kvm;
+        runAsRoot = false;
+
+        ovmf.enable = true;   # UEFI support
+        swtpm.enable = true;  # TPM for Windows 11
+      };
+    };
+
+    programs.virt-manager.enable = true;
+
+    virtualisation.spiceUSBRedirection.enable = true;
+
+    services.spice-vdagentd.enable = true;
+
+    users.users.${cfg.user}.extraGroups = [ "libvirtd" ];
+
+    environment.systemPackages = with pkgs; [
+      virt-manager
+      virt-viewer
+      spice
+      spice-gtk
+      win-virtio
+    ];
+
+    boot.kernelModules = [
+      "kvm-intel"
+      "kvm-amd"
+    ];
+  };
+}
